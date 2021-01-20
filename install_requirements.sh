e3001b3475f51d5cd51770eec6e6601aeb2d6db9
Claes Gill <claes@claesgill.com>
2021-01-20 21:04:04 +0100

    :fire::fire:

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Install Gnome desktop
+sudo apt-get -y update
+sudo apt-get -y upgrade
+#sudo apt-get -y -q=2  install xfce4 xfce4-goodies
+sudo apt-get -y -q=2  install ubuntu-gnome-desktop gnome-tweaks gnome-shell-extensions
+sudo apt-get -y -q=2  install xrdp
+sudo adduser xrdp ssl-cert
+sudo apt-get -y -q=2  install ca-certificates curl apt-transport-https lsb-release gnupg
+
 GREEN="\e[32m"
 YELLOW="\e[33m"
 RED="\e[91m"
